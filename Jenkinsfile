af357a8c0db3dfb89ae8479274a35590218c2ee6
Joseph Callen <jcallen@redhat.com>
2017-04-11 12:35:19 -0400

    cleanup

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,35 +8,37 @@ import com.redhat.*
 
 properties([disableConcurrentBuilds()])
 
-
-
-
 node {
     def source = ""
-
-    //echo sh(returnStdout: true, script: 'env')
-    println(scm.dump())
-    println(scm.branches[0])
-    println(scm.browser.url)
 
     String scmBranch = scm.branches[0]
     String scmUrl = scm.browser.url
 
+    /* if CHANGE_URL is defined then this is a pull request
+     * additional steps are required to determine the git url 
+     * and branch name to pass to new-build.
+     *
+     * Otherwise just use the scm.browser.url and scm.branches[0] 
+     * for new-build.
+     */
+
     if (env.CHANGE_URL) {
-        println(env.CHANGE_URL)
+        def pull = null
+        stage('Github Url and Ref') {
+            def changeUrl = env.CHANGE_URL
 
-        def changeUrl = env.CHANGE_URL
+            // Query the github repo api to return the clone_url and the ref (branch name)
+            def githubUri = changeUrl.replaceAll("github.com/", "api.github.com/repos/")
+            githubUri = githubUri.replaceAll("pull", "pulls")
 
-        // Query the github repo api to return the clone_url and the ref (branch name)
-        def githubUri = changeUrl.replaceAll("github.com/", "api.github.com/repos/")
-        githubUri = githubUri.replaceAll("pull", "pulls")
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "github" , usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            sh("curl -u ${env.USERNAME}:${env.PASSWORD} -o ${env.WORKSPACE}/github.json ${githubUri}")
-        }
-        def pull = readJSON file: 'github.json'
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "github", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                sh("curl -u ${env.USERNAME}:${env.PASSWORD} -o ${env.WORKSPACE}/github.json ${githubUri}")
+            }
+            pull = readJSON file: 'github.json'
 
-        if (pull.head.repo == null) {
-            error("Unable to read GitHub JSON file")
+            if (pull.head.repo == null) {
+                error("Unable to read GitHub JSON file")
+            }
         }
 
         newBuildOpenShift {
@@ -50,7 +52,6 @@ node {
             url = scmUrl
             branch = scmBranch
         }
-
     }
 }
 
--- vars/newBuildOpenShift.groovy
+++ vars/newBuildOpenShift.groovy
@@ -4,54 +4,51 @@ def call(Closure body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
-
-
-    println(body.getProperty('url'))
-    println(body.getProperty('branch'))
-
     body()
 
 
-    openshift.withCluster() {
-        openshift.withProject() {
-            try {
-                // use oc new-build to build the image using the clone_url and ref
-                newBuild = openshift.newBuild("${config.url}#${config.branch}")
-                echo "newBuild created: ${newBuild.count()} objects : ${newBuild.names()}"
-                def builds = newBuild.narrow("bc").related("builds")
+    def newBuild = null
 
-                timeout(10) {
-                    builds.watch {
-                        if (it.count() == 0) {
-                            return false
+    stage('OpenShift Build') {
+        openshift.withCluster() {
+            openshift.withProject() {
+                try {
+                    // use oc new-build to build the image using the clone_url and ref
+                    newBuild = openshift.newBuild("${config.url}#${config.branch}")
+                    echo "newBuild created: ${newBuild.count()} objects : ${newBuild.names()}"
+                    def builds = newBuild.narrow("bc").related("builds")
+
+                    timeout(10) {
+                        builds.watch {
+                            if (it.count() == 0) {
+                                return false
+                            }
+                            echo "Detected new builds created by buildconfig: ${it.names()}"
+                            return true
                         }
-                        echo "Detected new builds created by buildconfig: ${it.names()}"
-                        return true
-                    }
-                    builds.untilEach(1) {
-                        return it.object().status.phase == "Complete"
+                        builds.untilEach(1) {
+                            return it.object().status.phase == "Complete"
+                        }
                     }
                 }
-            }
-            finally {
-                if (newBuild) {
-                    def result = newBuild.narrow("bc").logs()
-                    echo "Result of logs operation:"
-                    echo "  status: ${result.status}"
-                    echo "  stderr: ${result.err}"
-                    echo "  number of actions to fulfill: ${result.actions.size()}"
-                    echo "  first action executed: ${result.actions[0].cmd}"
+                finally {
+                    if (newBuild) {
+                        def result = newBuild.narrow("bc").logs()
+                        echo "Result of logs operation:"
+                        echo "  status: ${result.status}"
+                        echo "  stderr: ${result.err}"
+                        echo "  number of actions to fulfill: ${result.actions.size()}"
+                        echo "  first action executed: ${result.actions[0].cmd}"
 
-                    if (result.status != 0) {
-                        echo "${result.out}"
-                        error("Image Build Failed")
+                        if (result.status != 0) {
+                            echo "${result.out}"
+                            error("Image Build Failed")
+                        }
+                        // After built we do not need the BuildConfig or the ImageStream
+                        newBuild.delete()
                     }
-                    // After built we do not need the BuildConfig or the ImageStream
-                    newBuild.delete()
                 }
             }
         }
     }
-
-
 }
