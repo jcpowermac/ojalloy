085c9e48d8a0866f5b21eddd12c365067abe89a9
Joseph Callen <jcallen@redhat.com>
2017-04-13 09:50:54 -0400

    testing locks

--- vars/newBuildOpenShift.groovy
+++ vars/newBuildOpenShift.groovy
@@ -12,19 +12,24 @@ def call(Closure body) {
         openshift.withCluster() {
             openshift.withProject() {
                 try {
+                    def builds = null
                     // use oc new-build to build the image using the clone_url and ref
-                    newBuild = openshift.newBuild("${config.url}#${config.branch}", "--name=${config.branch}")
-                    echo "newBuild created: ${newBuild.count()} objects : ${newBuild.names()}"
-                    def builds = newBuild.narrow("bc").related("builds")
+                    lock(resource: 'openshift.newBuild', inversePrecedence: true) {
+                        newBuild = openshift.newBuild("${config.url}#${config.branch}", "--name=${config.branch}")
+                        echo "newBuild created: ${newBuild.count()} objects : ${newBuild.names()}"
+                        builds = newBuild.narrow("bc").related("builds")
+                        timeout(5) {
+                            builds.watch {
+                                if (it.count() == 0) {
+                                    return false
+                                }
+                                echo "Detected new builds created by buildconfig: ${it.names()}"
+                                return true
+                            }
+                        }
+                    }
 
                     timeout(10) {
-                        builds.watch {
-                            if (it.count() == 0) {
-                                return false
-                            }
-                            echo "Detected new builds created by buildconfig: ${it.names()}"
-                            return true
-                        }
                         builds.untilEach(1) {
                             return it.object().status.phase == "Complete"
                         }
